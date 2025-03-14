b2885e0287075d7005619c699128cb56fd469b9f
YamazakiGuigong <166939454+YamazakiGuigong@users.noreply.github.com>
2025-03-14 23:50:56 +0800

    TCS5.0
    优化了食物生成的卡顿，解决了避免调用main函数来重新游戏，现在的问题是蛇满屏之后无法取得胜利

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -8,7 +8,6 @@
 
 using namespace std;
 
-
 int length = 2, score = 0;
 int rows, cols;
 vector<vector<int>> sequence;
@@ -39,27 +38,19 @@ void print()
 	SetConsoleCursorInfo(handle, &curInfo);
 
 	for (int i = 1; i < rows - 1; i++)
-	{
 		for (int j = 1; j < cols - 1; j++)
 		{
-			if (sequence[i][j] == -1)
-				map[i][j] = '*';
-
-			else if (sequence[i][j] == 0)
-				map[i][j] = ' ';
-
-			else if (sequence[i][j] == 1)
-				map[i][j] = 'S';
-
-			else if (sequence[i][j] > 1)
-				map[i][j] = 'O';
+			switch (sequence[i][j])
+			{
+			case -1 : map[i][j] = '*';; break;
+			case 0: map[i][j] = ' '; break;	
+			case 1: map[i][j] = 'S'; break;
+			default:if(sequence[i][j] > 1 ) map[i][j] = 'O'; break;
+			}
 
 		}
-		//Sleep(5);
-	}
 
 	for (int i = 0; i < rows; i++)
-	{
 		for (int j = 0; j < cols; j++)
 		{
 			pos.X = j;
@@ -67,7 +58,7 @@ void print()
 			SetConsoleCursorPosition(handle, pos);
 			cout << map[i][j];
 		}
-	}
+
 	pos.X = 0;
 	pos.Y = rows;
 	SetConsoleCursorPosition(handle, pos);
@@ -77,7 +68,6 @@ void print()
 void generateFood(int& foodx, int& foody)
 {
 	do {
-		srand(unsigned(time(NULL)));
 		foodx = 1 + rand() % (rows - 2);
 		foody = 1 + rand() % (cols - 2);
 	} while (sequence[foodx][foody] != 0);
@@ -87,27 +77,29 @@ void generateFood(int& foodx, int& foody)
 
 int main()
 {
-	int x, y, t,num,book,cache;
+	srand(unsigned(time(NULL)));
+
+	int x, y, t,book,cache,i, j, life;;
 	float FPS = 1000 / 120.0f;
 	int startTime = 0;
 	int frameTime = 0;
 	int endTime = 0;
+	char direction = 'a';
+
 	cout << "输入行数和列数：\n";
 	cin >> rows >> cols;
 	sequence.resize(rows, vector<int>(cols, 0));
 	initMap();
 
-	x = rows / 2, y = cols / 2, t = 0,num=0,book=0,cache=0;
+	x = rows / 2, y = cols / 2, t = 0, cache = 0, life=1;
 	sequence[x][y] = 1;
 	sequence[x][y + 1] = 2;
 
 	int foodx, foody;
 	generateFood(foodx, foody);
 
-	char direction = 'a';
-	bool alive = true;
-	int i, j;
-	while (alive)
+	
+	while (life)
 	{
 		startTime = clock();
 		t++;
@@ -127,11 +119,9 @@ int main()
 				}
 			}
 		}
-			if (t % 15 == 0 || num > 0)
+			if (t % 15 == 0)
 			{
 				cache = 0;
-				if (num > 0)
-					num--;
 
 				int newX = x, newY = y;
 				switch (direction)
@@ -144,7 +134,7 @@ int main()
 
 				if (newX <= 0 || newX >= rows - 1 || newY <= 0 || newY >= cols - 1 || sequence[newX][newY] > 0)
 				{
-					alive = false;
+					life = 0;
 					break;
 				}
 
@@ -161,16 +151,18 @@ int main()
 						score = length;
 
 					generateFood(foodx, foody);
-					endTime = clock() - startTime;
-					book = book + endTime;
-					while (book >= 8)
+					endTime = clock() - startTime;//计算单次食物生成时间
+					while (endTime >= 8)//如果食物生成时间大于8ms，执行t+1.
 					{
-						book /= 8;
-						num++;
+						endTime /= 8;
+						t++;
+						if (t % 15 == 0)
+							t = 14;
+						if (t % 30 == 0)
+							t = 14;
 					}
 				}
-				else
-				{
+				else{
 					for (i = 1; i < rows - 1; i++)
 						for (j = 1; j < cols - 1; j++)
 						{
@@ -195,12 +187,17 @@ int main()
 		frameTime = clock() - startTime;
 		if (frameTime > 0 && FPS > frameTime)
 			Sleep(FPS - frameTime);
-		if (t == 30)
+		if (t == 31)
 			t = 0;
+		if (length == (rows - 2) * (cols - 2))
+			break;
 	}
 
 	system("cls");
+	if(life==0)
 	cout << "Game Over! Final length: " << length << endl;
+	else
+		cout << "You win! Final length: " << length << endl;
 	cout << "Press R to restart, any other key to exit...";
 
 	if (tolower(_getch()) == 'r')
